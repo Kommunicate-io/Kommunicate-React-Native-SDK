021676fa3f0d83c18d75761f5c0dca09810fa6a0
Sathyan-KM <sathyan.elangovan@kommunicate.io>
2023-01-30 18:30:18 +0530

    fix build issue

--- ios/Bridge.m
+++ ios/Bridge.m
@@ -1,5 +1,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+
 
 @interface RCT_EXTERN_MODULE(RNEventEmitter, RCTEventEmitter)
 
--- ios/RNEventEmitter.swift
+++ ios/RNEventEmitter.swift
@@ -2,14 +2,45 @@
 
 
 
-
+import React
 import Kommunicate
 import KommunicateChatUI_iOS_SDK
 import KommunicateCore_iOS_SDK
-import RCTEventEmitter
 
 @objc(RNEventEmitter)
 open class RNEventEmitter: RCTEventEmitter, ALKCustomEventCallback {
+    public func messageSent(message: ALMessage) {
+        
+    }
+    
+    public func messageReceived(message: ALMessage) {
+        
+    }
+    
+    public func conversationRestarted(converstionId: String) {
+        
+    }
+    
+    public func onBackButtonClick(isConversationOpened: Bool) {
+        
+    }
+    
+    public func faqClicked(url: String) {
+        
+    }
+    
+    public func conversationCreated(conversationId: String) {
+        
+    }
+    
+    public func ratingSubmitted(conversationId: String, rating: Int, comment: String) {
+        
+    }
+    
+    public func richMessageClicked(conversationId: String, action: [String : Any], type: String) {
+        
+    }
+    
 
   public static var emitter: RCTEventEmitter!
 
