8973a5d7381f2f97e3cbe71db0ca7b243e83b123
Sathyan-KM <sathyan.elangovan@kommunicate.io>
2023-01-30 17:43:30 +0530

    Added emitter class

---
+++ ios/Bridge.m
@@ -0,0 +1,7 @@
+
+
+@interface RCT_EXTERN_MODULE(RNEventEmitter, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(supportedEvents)
+
+@end
---
+++ ios/RNEventEmitter.swift
@@ -0,0 +1,27 @@
+
+
+
+
+
+import Kommunicate
+import KommunicateChatUI_iOS_SDK
+import KommunicateCore_iOS_SDK
+
+@objc(RNEventEmitter)
+open class RNEventEmitter: RCTEventEmitter, ALKCustomEventCallback {
+
+  public static var emitter: RCTEventEmitter!
+
+  override init() {
+    super.init()
+    RNEventEmitter.emitter = self
+  }
+
+  open override func supportedEvents() -> [String] {
+    ["onPluginLaunch", "onPending"]      // etc.
+  }
+
+  
+
+
+}
