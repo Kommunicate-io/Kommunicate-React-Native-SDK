d271d7a67ef779c1a2f945b88afe7faa375bfd72
Sathyan-KM <sathyan.elangovan@kommunicate.io>
2023-01-31 10:45:12 +0530

    added event callbacks on RNEventEmitter

--- ios/RNEventEmitter.swift
+++ ios/RNEventEmitter.swift
@@ -11,52 +11,60 @@ import KommunicateCore_iOS_SDK
 open class RNEventEmitter: RCTEventEmitter, ALKCustomEventCallback {
     public func messageSent(message: ALMessage) {
         print("pakka101 \(message.message)")
-        RNEventEmitter.emitter.sendEvent(withName: "onPluginLaunch", body: nil)
+        RNEventEmitter.emitter.sendEvent(withName: "onMessageSent", body: nil)
+
     }
     
     public func messageReceived(message: ALMessage) {
         print("pakka101 \(message.message)")
+        // let encodedData = try JSONEncoder().encode(message)
+        // let jsonString = String(data: encodedData,
+                                encoding: .utf8)
+        RNEventEmitter.emitter.sendEvent(withName: "onMessageReceived", body: nil)
+
     }
     
     public func conversationRestarted(converstionId: String) {
-        
+      RNEventEmitter.emitter.sendEvent(withName: "onConversationRestarted", body: nil)
+
     }
     
     public func onBackButtonClick(isConversationOpened: Bool) {
-        
+      RNEventEmitter.emitter.sendEvent(withName: "onBackButtonClicked", body: nil)
+
     }
     
     public func faqClicked(url: String) {
-        
+        RNEventEmitter.emitter.sendEvent(withName: "onFaqClick", body: nil)
+
     }
     
     public func conversationCreated(conversationId: String) {
-        
+      RNEventEmitter.emitter.sendEvent(withName: "onStartNewConversation", body: nil)
+
     }
     
     public func ratingSubmitted(conversationId: String, rating: Int, comment: String) {
-        
+        RNEventEmitter.emitter.sendEvent(withName: "onSubmitRatingClick", body: nil)
     }
     
     public func richMessageClicked(conversationId: String, action: [String : Any], type: String) {
-        
+        RNEventEmitter.emitter.sendEvent(withName: "onRichMessageButtonClick", body: nil)
     }
     
 
   public static var emitter: RCTEventEmitter!
 
   override init() {
-    super.init()
+//    super.init()
+      super.init(disabledObservation: ())
     RNEventEmitter.emitter = self
-      Kommunicate.subscribeCustomEvents(events: [CustomEvent.messageReceive, CustomEvent.messageSend], callback: self)
+      Kommunicate.subscribeCustomEvents(events: [CustomEvent.messageReceive, CustomEvent.messageSend,CustomEvent.faqClick, CustomEvent.conversationBackPress, CustomEvent], callback: self)
       print("Pakka101 entering")
   }
 
   open override func supportedEvents() -> [String] {
-    ["onPluginLaunch", "onPending"]      // etc.
+      ["onMessageReceived", "onMessageSent", "onConversationRestarted", "onRichMessageButtonClick", "onStartNewConversation", "onSubmitRatingClick", "onBackButtonClicked", "onFaqClick", "onConversationRestarted" ]      // etc.
   }
-
-  
-
 
 }
